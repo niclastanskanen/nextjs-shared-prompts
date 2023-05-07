785b571c5dbdd3f7c26eb48ee920424dd2f68647
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-05-07 14:20:23 +0200

    add logged in navbar to show buttons and profile picture

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from '@components/Nav';
 import Provider from '@components/Provider';
 
 export const metadata = {
-    title: "ClearifyAi",
+    title: "clearyfAI",
     description: "Discover & Share AI Prompts"
 }
 
