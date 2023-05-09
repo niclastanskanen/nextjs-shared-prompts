d77279df3cca40b79b300070304b454926435ef7
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-05-09 10:25:30 +0200

    add API end point for create prompt

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,11 @@
+import { connectToDb } from "@utils/database";
+
+export const POST = async (req) => {
+    const { userId, prompt, tag } = await req.json();
+
+    try {
+        await connectToDb();
+    } catch (error) {
+        
+    }
+}
