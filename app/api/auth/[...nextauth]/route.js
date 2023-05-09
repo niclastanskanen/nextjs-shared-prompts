48ff7e44cc42c3c8837577c7312fba477c864de6
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-05-09 09:08:24 +0200

    add mongoDB connected in api

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
+import { connectToDb } from "@utils/database";
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
@@ -13,9 +15,13 @@ const handler = NextAuth({
     },
     async signIn({ profile }) {
         try {
+            await connectToDb();
             
+
+            return true;
         } catch (error) {
-            
+            console.log(error);
+            return false;
         }
     }
 })
