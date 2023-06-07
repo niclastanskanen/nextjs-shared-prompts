a465d85757181bfee895fd56531cc25acd45084b
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-07 11:44:10 +0200

    add updatePrompt function

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -20,39 +20,40 @@ const EditPrompt = () => {
         const getPromptDetails = async () => {
           const response = await fetch(`/api/prompt/${promptId}`);
           const data = await response.json();
-    
+
           setPost({
             prompt: data.prompt,
             tag: data.tag,
           });
         };
-    
+
         if (promptId) getPromptDetails();
       }, [promptId]);
 
-//   const createPrompt = async (e) => {
-//     e.preventDefault();
-//     setIsSubmitting(true);
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
-//     try {
-//       const response = await fetch("/api/prompt/new", {
-//         method: "POST",
-//         body: JSON.stringify({
-//           prompt: post.prompt,
-//           userId: session?.user.id,
-//           tag: post.tag,
-//         }),
-//       });
+    if(!promptId) return alert('Prompt ID not found')
 
-//       if (response.ok) {
-//         router.push("/");
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     } finally {
-//       setIsSubmitting(false);
-//     }
-//   };
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
@@ -60,7 +61,7 @@ const EditPrompt = () => {
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={() => {}}
+      handleSubmit={updatePrompt}
     />
   );
 };
