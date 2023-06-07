b76830fce97bab64873368410d0f9432cb5b2b9f
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-07 12:44:21 +0200

    add search function

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -50,7 +50,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         </div>
       </div>
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-      <p 
+      <p
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
