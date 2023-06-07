7c035568f7e324ec2903d81fe25bd2909bab072c
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-07 11:12:04 +0200

    add prompt id route from API

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,8 +6,10 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
-  console.log(post)
   const [copied, setCopied] = useState('');
 
   const handleCopy = () => {
@@ -28,7 +30,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             className='rounded-full object-contain'
           />
           <div className="flex flex-col">
-            <h3 className="font-satosi font-semibold text-gray-900">
+            <h3 className="font-satoshi font-semibold text-gray-900">
               {post.creator?.username}
             </h3>
             <p className="font-inter text-sm text-gray-500">
@@ -54,6 +56,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
